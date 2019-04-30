6030819cdf80629bb818d626b2ba82c8f3447ff4
Christian Søndergaard Weatherley <alandryl@live.dk>
2019-04-30 20:49:15 +0200

    added first player controller script and camera movement.

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing = 5;
+
+    public Vector3 offset;
+
+    public float camPositionClamp = 3;
+
+    public float moveOffsetValue;
+    bool facingRight;
+
+    void Start()
+    {
+        transform.position = target.position + offset;
+
+        facingRight = true;
+    }
+
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        Vector3 cameraOffset = new Vector3(offset.x + moveOffsetValue, offset.y, offset.z);
+
+        Vector3 targetCamPos = target.position + cameraOffset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+
+
+
+        if (move > 0 && !facingRight)
+        {
+            moveOffsetValue *= -1;
+            facingRight = true;
+        }
+        else if (move < 0 && facingRight)
+        {
+            moveOffsetValue *= -1;
+            facingRight = false;
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject model;
+
+    public float runSpeed;
+
+
+    bool facingRight;
+
+    Rigidbody rb;
+    Animator ac;
+
+    //Jumping
+    bool grounded = false;
+    Collider[] groundCollisions;
+    float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float jumpHeight;
+
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        ac = model.GetComponent<Animator>();
+
+        facingRight = true;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+        //Jumping
+
+        if(grounded && Input.GetAxis("Jump") > 0)
+        {
+            grounded = false;
+            ac.SetBool("Grounded", grounded);
+            rb.AddForce(new Vector3(0, jumpHeight, 0));
+        }
+
+        //Grounded
+
+        groundCollisions = Physics.OverlapSphere(groundCheck.position, groundCheckRadius, groundLayer);
+        if (groundCollisions.Length > 0)
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+
+        ac.SetBool("Grounded", grounded);
+
+        //Walking
+
+        float move = Input.GetAxis("Horizontal");
+        ac.SetFloat("Speed", Mathf.Abs(move));
+
+        rb.velocity = new Vector3(move * runSpeed, rb.velocity.y, 0);
+
+        //Jumping
+
+
+
+        //Flip
+
+        if (move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        model.transform.localRotation *= Quaternion.Euler(0, 180, 0);
+    }
+}
