741633861e8d5469113ef897482cb9b2ed148615
Christian Søndergaard Weatherley <alandryl@live.dk>
2019-05-06 18:34:12 +0200

    added main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string playScene;
+    public GameObject BlackScreenFadeObject;
+
+    bool disableSelection;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame ()
+    {
+        if (!disableSelection)
+        {
+            StartCoroutine(StartingGame());
+        }
+    }
+
+    public void QuitGame ()
+    {
+        if (!disableSelection)
+        {
+            Application.Quit();
+        }
+    }
+
+    IEnumerator StartingGame()
+    {
+        disableSelection = true;
+        BlackScreenFadeObject.GetComponent<Animator>().SetTrigger("Fade");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(playScene);
+    }
+
+}
