9ed0cd63319c7db147f735e675dd05dbfcaa99d3
Christian Søndergaard Weatherley <alandryl@live.dk>
2019-05-06 21:40:16 +0200

    pause menu added

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    public string mainMenuScene;
+    public GameObject BlackScreenFadeObject;
+
+    bool disableSelection;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void ReturnToMainMenu()
+    {
+        if (!disableSelection)
+        {
+            Resume();
+            StartCoroutine(ReturningToMainMenu());
+        }
+    }
+
+    public void QuitGame()
+    {
+        if (!disableSelection)
+        {
+            Application.Quit();
+        }
+    }
+
+    IEnumerator ReturningToMainMenu()
+    {
+        disableSelection = true;
+        BlackScreenFadeObject.GetComponent<Animator>().SetTrigger("Fade");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(mainMenuScene);
+    }
+
+}
