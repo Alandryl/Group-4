95cbadebf2b2bfef666f2672085e48d26c8c1c69
Christian Søndergaard Weatherley <alandryl@live.dk>
2019-05-21 09:56:42 +0200

    added player light script

---
+++ Assets/PlayerLight.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLight : MonoBehaviour
+{
+    Rigidbody rb;
+    public GameObject lightObject;
+    public LayerMask darkLayer;
+
+    public bool isInDark;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (isInDark)
+        {
+            lightObject.SetActive(true);
+        }
+        else
+        {
+            lightObject.SetActive(false);
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.layer == 10)
+        {
+            isInDark = true;
+        }
+        else
+        {
+            isInDark = false;
+        }
+
+    }
+    
+    /*
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.layer == darkLayer)
+        {
+            isInDark = true;
+            Debug.Log("Working");
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.layer == darkLayer)
+        {
+            isInDark = false;
+        }
+    }
+    */
+}
