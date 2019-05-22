2b81cd0cd4e976022fe72e3f454c25e3eb7e52b6
Christian Søndergaard Weatherley <alandryl@live.dk>
2019-05-22 12:12:40 +0200

    updated player

---
+++ Assets/PlayerSounds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSounds : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    [Header("Audio")]
+    public AudioClip audioFootstep;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Footstep()
+    {
+        audioSource.PlayOneShot(audioFootstep);
+    }
+}
